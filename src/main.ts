c07b06ffe605fe29c45c07bbbc29dfb4d22c742a
Germain <katakugermain@gmail.com>
2023-05-16 08:43:34 +0200

    fixing enve

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,9 +14,7 @@ async function bootstrap() {
     credentials: true
   });
 
-  const configService: ConfigService = app.get<ConfigService>('ConfigService');
-
-  const port: number = configService.get<number>('port');
-  await app.listen(port);
+  const config = app.get("ConfigService");
+  await app.listen(config.get("port"));
 }
 bootstrap();
